94147ce7732ac720dbd6814647f427e74030c617
Olli-Pekka Heinisuo <op.heinisuo@gmail.com>
2016-04-08 21:16:15 +0300

    initial appveyor template

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ import os
 import sys
 
 opencv_version = ""
+binary_path = ""
 
-if "--opencv_version" in sys.argv:
+if "--opencv-version" in sys.argv:
     index = sys.argv.index('--opencv-version')
     sys.argv.pop(index)
     opencv_version = sys.argv.pop(index)
@@ -12,12 +13,20 @@ else:
     print("Error: no version info (--opencv-version missing), exiting.")
     exit(1)
 
+if "--binary-path" in sys.argv:
+    index = sys.argv.index('--binary-path')
+    sys.argv.pop(index)
+    binary_path = sys.argv.pop(index)
+else:
+    print("Error: Binary path not provided (--binary-path missing), exiting.")
+    exit(1)
+
 package_data = {}
 
 if os.name == 'posix':
-    package_data['cv2'] = ['*.so']
+    package_data['cv2'] = ['%s\*.so' % binary_path]
 else:
-    package_data['cv2'] = ['*.pyd']
+    package_data['cv2'] = ['%s\*.pyd' % binary_path]
 
 setup(name='opencv-python',
       version=opencv_version,
