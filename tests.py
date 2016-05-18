2bec314827e0c38257a3d6bdfa87c8d85aeecf8f
Olli-Pekka Heinisuo <skvark@users.noreply.github.com>
2016-05-18 22:46:29 +0300

    Delete tests.py

--- tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class OpenCVTest(unittest.TestCase):
-    """ Simple functionality tests. """
-
-    def test_import(self):
-        """ Test that the cv2 module can be imported. """
-        import cv2
