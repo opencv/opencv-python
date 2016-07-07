55fee0fe4bc80d94d8d913a81c42ec9225fdfb1d
Carlos Martinez <neocarlitos@gmail.com>
2016-07-07 21:56:42 +0200

    Update following suggestions from opencv #6774

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,7 +3,7 @@
 /opt/_internal/cpython-3.5.1/bin/pip3.5 install --upgrade git+git://github.com/pypa/auditwheel
 
 cd /io
-git clone -q --branch=python-wheel https://github.com/c-martinez/opencv.git opencv
+git clone -q --branch=master https://github.com/Itseez/opencv.git opencv
 
 for PYBIN in /opt/python/cp$PYTHON_VERSION*/bin; do
     $PYBIN/python find_version.py
@@ -13,17 +13,31 @@ for PYBIN in /opt/python/cp$PYTHON_VERSION*/bin; do
     cd opencv
     mkdir build
     if [[ $PYTHON_VERSION == 2* ]]; then
-      cmake28 -H"." -B"build" -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_opencv_python3=OFF -DBUILD_opencv_java=OFF -DBUILD_SHARED_LIBS=OFF  -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DPYTHON_EXECUTABLE=$PYBIN/python -DPYTHON_INCLUDE_DIR=$($PYBIN/python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") -DPYTHON_PACKAGES_PATH=$($PYBIN/python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") -Wno-dev;
+      cmake28 -H"." -B"build" -DCMAKE_BUILD_TYPE=Release -DBUILD_opencv_python3=OFF -DBUILD_opencv_java=OFF -DBUILD_SHARED_LIBS=OFF \
+        -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF \
+        -DPYTHON2INTERP_FOUND=ON -DPYTHON2LIBS_FOUND=ON \
+        -DPYTHON2_EXECUTABLE=$PYBIN/python \
+        -DPYTHON2_VERSION_STRING=$($PYBIN/python -c "from platform import python_version; print python_version()") \
+        -DPYTHON2_INCLUDE_PATH=$($PYBIN/python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
+        -DPYTHON2_PACKAGES_PATH=$($PYBIN/python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
+        -DPYTHON2_NUMPY_INCLUDE_DIRS=$($PYBIN/python -c "import os; os.environ['DISTUTILS_USE_SDK']='1'; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))") \
+        -DPYTHON2_NUMPY_VERSION=$($PYBIN/python -c "import numpy; print(numpy.version.version)")
+      (cd build; make -j5 opencv_python2)
     fi
 
     if [[ $PYTHON_VERSION == 3* ]]; then
-      cmake28 -H"." -B"build" -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_opencv_python2=OFF -DBUILD_opencv_java=OFF -DBUILD_SHARED_LIBS=OFF  -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DPYTHON3_EXECUTABLE=$PYBIN/python -DPYTHON_INCLUDE_DIR=$($PYBIN/python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") -DPYTHON_PACKAGES_PATH=$($PYBIN/python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") -Wno-dev;
+      cmake28 -H"." -B"build" -DCMAKE_BUILD_TYPE=Release -DBUILD_opencv_python2=OFF -DBUILD_opencv_java=OFF -DBUILD_SHARED_LIBS=OFF \
+        -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF \
+        -DPYTHON3INTERP_FOUND=ON -DPYTHON3LIBS_FOUND=ON \
+        -DPYTHON3_EXECUTABLE=$PYBIN/python \
+        -DPYTHON3_VERSION_STRING=$($PYBIN/python -c "from platform import python_version; print python_version()") \
+        -DPYTHON3_INCLUDE_PATH=$($PYBIN/python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
+        -DPYTHON3_PACKAGES_PATH=$($PYBIN/python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
+        -DPYTHON3_NUMPY_INCLUDE_DIRS=$($PYBIN/python -c "import os; os.environ['DISTUTILS_USE_SDK']='1'; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))") \
+        -DPYTHON3_NUMPY_VERSION=$($PYBIN/python -c "import numpy; print(numpy.version.version)")
+      (cd build; make -j5 opencv_python3)
     fi
-
-    # DO BUILD
-    cd build
-    cmake28 --build . --config Release
-    cd ../..
+    cd ..
 
     if [[ $PYTHON_VERSION == 2* ]]; then
       cp opencv/build/lib/cv2.so cv2/
@@ -32,10 +46,13 @@ for PYBIN in /opt/python/cp$PYTHON_VERSION*/bin; do
     if [[ $PYTHON_VERSION == 3* ]]; then
       cp opencv/build/lib/python3/*.so cv2/
     fi
-    rm -fr opencv/build
 
     # Build wheel
     $PYBIN/pip wheel . -w tmpwheels/
+
+    # Cleanup
+    rm -fr opencv/build
+    rm cv2/*.so
 done
 
 # Bundle external shared libraries into the wheels
