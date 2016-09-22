aef602074c3c4a4d0094a56fb96e60a8a22a36c1
Olli-Pekka Heinisuo <op.heinisuo@gmail.com>
2016-09-22 19:05:00 +0300

    fix indentation

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,7 @@ class OpenCVTest(unittest.TestCase):
         import cv2
 
     def test_video_capture(self):
-    	import cv2
+        import cv2
 
-		cap = cv2.VideoCapture("SampleVideo_1280x720_1mb.mp4")
-		self.assertTrue(cap.isOpened())
+        cap = cv2.VideoCapture("SampleVideo_1280x720_1mb.mp4")
+        self.assertTrue(cap.isOpened())
