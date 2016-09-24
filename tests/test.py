98e385e400279498047cfc8aa92912dc2f2059c5
Olli-Pekka Heinisuo <op.heinisuo@gmail.com>
2016-09-24 13:52:37 +0300

    Add FFmpeg license, inject cv2 to PATH in __init__, enable video test

--- tests/test.py
+++ tests/test.py
@@ -7,10 +7,8 @@ class OpenCVTest(unittest.TestCase):
         """ Test that the cv2 module can be imported. """
         import cv2
 
-    """
     def test_video_capture(self):
         import cv2
 
         cap = cv2.VideoCapture("SampleVideo_1280x720_1mb.mp4")
         self.assertTrue(cap.isOpened())
-    """
