9d43f841b4b45c3261fcb0cd235784c8b17e2fba
glitchassassin <jon.winsley@gmail.com>
2016-12-27 13:15:41 -0500

    Rearranged path modification

--- cv2/__init__.py
+++ cv2/__init__.py
@@ -1,8 +1,9 @@
 import sys
 import os
 
-# FFMPEG dll is not found on Windows without this
-#os.environ["PATH"] += os.pathsep + os.path.dirname(os.path.realpath(__file__))
-
 from . import cv2
 sys.modules['cv2'] = cv2
+
+
+# FFMPEG dll is not found on Windows without this
+os.environ["PATH"] += os.pathsep + os.path.dirname(os.path.realpath(__file__))
