75e663b944397858ca4e0ca61c459f5e31abbd08
Olli-Pekka Heinisuo <op.heinisuo@gmail.com>
2017-03-06 19:36:27 +0200

    modify the execution rights

--- travis/install-ninja.sh
+++ travis/install-ninja.sh
@@ -1,2 +1,7 @@
-wget https://github.com/ninja-build/ninja/releases/download/v1.7.2/ninja-linux.zip -O ninja.zip; unzip ninja.zip; rm ninja.zip
+wget https://github.com/ninja-build/ninja/releases/download/v1.7.2/ninja-linux.zip -O ninja.zip
+unzip ninja.zip
+rm ninja.zip
+chmod +x ninja
+ls -la
 export PATH=$PATH:$(pwd)
+echo $PATH
