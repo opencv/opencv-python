3186d5aa524dc805a7543b59aa9ee9378a64a0c1
Olli-Pekka Heinisuo <op.heinisuo@gmail.com>
2017-03-06 20:11:36 +0200

    back to manual installation

---
+++ travis/install-ninja.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://github.com/ninja-build/ninja/releases/download/v1.7.2/ninja-linux.zip -O ninja.zip
+unzip ninja.zip
+rm ninja.zip
+chmod o+r ninja
+ls -la
+export PATH=$PATH:$(pwd)
+echo $PATH
