8ca2cf858798e2187705ce49e47808548769a7e8
Olli-Pekka Heinisuo <op.heinisuo@gmail.com>
2017-08-11 16:34:36 +0300

    unify and clean up Linux and macOS build scripts, detect python paths automatically on macOS

--- travis/build-wheels-osx.sh
+++ travis/build-wheels-osx.sh
@@ -4,7 +4,7 @@ echo 'Begin build-wheel OSX ...'
 
 export PYTHON_VERSION=${MB_PYTHON_VERSION/./}
 echo 'MB_PYTHON_VERSION: ' $MB_PYTHON_VERSION
-echo 'PYTHON_VERSION: '$PYTHON_VERSION
+echo 'PYTHON_VERSION: ' $PYTHON_VERSION
 
 echo 'PIP and brew installs'
 
@@ -16,6 +16,20 @@ echo "Apply patch"
 
 git apply --ignore-space-change --ignore-whitespace ../travis/disable_i386.patch
 
+echo "Detect Python paths for OpenCV"
+
+PYTHON_VERSION_STRING = $(python -c "from platform import python_version; print(python_version())")
+PYTHON_INCLUDE_PATH = $(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
+PYTHON_PACKAGES_PATH = $(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
+PYTHON_NUMPY_INCLUDE_DIRS = $(python -c "import os; os.environ['DISTUTILS_USE_SDK']='1'; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))")
+PYTHON_NUMPY_VERSION = $(python -c "import numpy; print(numpy.version.version)")
+
+echo "PYthon version string: $PYTHON_VERSION_STRING"
+echo "Python include path: $PYTHON_INCLUDE_PATH"
+echo "Python packages path: $PYTHON_PACKAGES_PATH"
+echo "Python numpy incude dirs: $PYTHON_NUMPY_INCLUDE_DIRS"
+echo "Python numpy version: $PYTHON_NUMPY_VERSION"
+
 echo 'Config make'
 
 mkdir build
@@ -25,94 +39,77 @@ if [[ $PYTHON_VERSION == 2* ]] && [[ $ENABLE_CONTRIB == 0 ]]; then
   echo 'Config for Py2'
   cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D CMAKE_TOOLCHAIN_FILE=../../travis/toolchain_macos.cmake \
     -D BUILD_opencv_python3=OFF -D BUILD_opencv_java=OFF -D BUILD_SHARED_LIBS=OFF -D WITH_LAPACK=OFF \
-    -D PYTHON2_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-  	-D PYTHON2_LIBRARY=/usr/local/Cellar/python/2.7.10/Frameworks/Python.framework/Versions/2.7/bin \
-  	-D PYTHON2_INCLUDE_DIR=/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 \
-  	-D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
-  	-D BUILD_EXAMPLES=OFF ..
+    -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
+    -D BUILD_EXAMPLES=OFF \
+    -D PYTHON2INTERP_FOUND=ON -DPYTHON2LIBS_FOUND=ON \
+    -D PYTHON2_EXECUTABLE=python \
+    -D PYTHON2_VERSION_STRING=$PYTHON_VERSION_STRING \
+    -D PYTHON2_INCLUDE_PATH=$PYTHON_INCLUDE_PATH \
+    -D PYTHON2_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
+    -D PYTHON2_NUMPY_INCLUDE_DIRS=$PYTHON_NUMPY_INCLUDE_DIRS \
+    -D PYTHON2_NUMPY_VERSION=$PYTHON_NUMPY_VERSION \
+    ..
 fi
 
-if [[ $PYTHON_VERSION == 34 ]] && [[ $ENABLE_CONTRIB == 0 ]]; then
-  echo 'Config for Py34'
+if [[ $PYTHON_VERSION == 3* ]] && [[ $ENABLE_CONTRIB == 0 ]]; then
+  echo 'Config for Py3'
   cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D CMAKE_TOOLCHAIN_FILE=../../travis/toolchain_macos.cmake \
     -D BUILD_opencv_python2=OFF -D BUILD_opencv_java=OFF -D BUILD_SHARED_LIBS=OFF -D WITH_LAPACK=OFF \
-    -D PYTHON3_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-    -D PYTHON3_LIBRARY=/usr/local/Cellar/python3/3.4.2_1/Frameworks/Python.framework/Versions/3.4/bin \
-    -D PYTHON3_INCLUDE_DIR=/Library/Frameworks/Python.framework/Versions/3.4/include/python3.4m \
     -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
-    -D BUILD_EXAMPLES=OFF ..
+    -D BUILD_EXAMPLES=OFF \
+    -D PYTHON3INTERP_FOUND=ON -DPYTHON3LIBS_FOUND=ON \
+    -D PYTHON3_EXECUTABLE=python \
+    -D PYTHON3_VERSION_STRING=$PYTHON_VERSION_STRING \
+    -D PYTHON3_INCLUDE_PATH=$PYTHON_INCLUDE_PATH \
+    -D PYTHON3_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
+    -D PYTHON3_NUMPY_INCLUDE_DIRS=$PYTHON_NUMPY_INCLUDE_DIRS \
+    -D PYTHON3_NUMPY_VERSION=$PYTHON_NUMPY_VERSION \
+    ..
 fi
-
-if [[ $PYTHON_VERSION == 35 ]] && [[ $ENABLE_CONTRIB == 0 ]]; then
-  echo 'Config for Py35'
-  cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D CMAKE_TOOLCHAIN_FILE=../../travis/toolchain_macos.cmake \
-    -D BUILD_opencv_python2=OFF -D BUILD_opencv_java=OFF -D BUILD_SHARED_LIBS=OFF -D WITH_LAPACK=OFF \
-    -D PYTHON3_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-    -D PYTHON3_LIBRARY=/usr/local/Cellar/python3/3.5.1/Frameworks/Python.framework/Versions/3.5/bin \
-    -D PYTHON3_INCLUDE_DIR=/Library/Frameworks/Python.framework/Versions/3.5/include/python3.5m \
-    -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
-    -D BUILD_EXAMPLES=OFF ..
-fi
-
-if [[ $PYTHON_VERSION == 36 ]] && [[ $ENABLE_CONTRIB == 0 ]]; then
-  echo 'Config for Py36'
-  cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D CMAKE_TOOLCHAIN_FILE=../../travis/toolchain_macos.cmake \
-    -D BUILD_opencv_python2=OFF -D BUILD_opencv_java=OFF -D BUILD_SHARED_LIBS=OFF -D WITH_LAPACK=OFF \
-    -D PYTHON3_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-    -D PYTHON3_LIBRARY=/usr/local/Cellar/python3/3.6.0/Frameworks/Python.framework/Versions/3.6/bin \
-    -D PYTHON3_INCLUDE_DIR=/Library/Frameworks/Python.framework/Versions/3.6/include/python3.6m \
-    -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
-    -D BUILD_EXAMPLES=OFF ..
-fi
-
 
 if [[ $PYTHON_VERSION == 2* ]] && [[ $ENABLE_CONTRIB == 1 ]]; then
   echo 'Config for Py2'
   cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D CMAKE_TOOLCHAIN_FILE=../../travis/toolchain_macos.cmake -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
     -D BUILD_opencv_python3=OFF -D BUILD_opencv_java=OFF -D BUILD_SHARED_LIBS=OFF -D WITH_LAPACK=OFF \
-    -D PYTHON2_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-    -D PYTHON2_LIBRARY=/usr/local/Cellar/python/2.7.10/Frameworks/Python.framework/Versions/2.7/bin \
-    -D PYTHON2_INCLUDE_DIR=/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 \
     -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
-    -D BUILD_EXAMPLES=OFF ..
+    -D BUILD_EXAMPLES=OFF \
+    -D PYTHON2INTERP_FOUND=ON -DPYTHON2LIBS_FOUND=ON \
+    -D PYTHON2_EXECUTABLE=python \
+    -D PYTHON2_VERSION_STRING=$PYTHON_VERSION_STRING \
+    -D PYTHON2_INCLUDE_PATH=$PYTHON_INCLUDE_PATH \
+    -D PYTHON2_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
+    -D PYTHON2_NUMPY_INCLUDE_DIRS=$PYTHON_NUMPY_INCLUDE_DIRS \
+    -D PYTHON2_NUMPY_VERSION=$PYTHON_NUMPY_VERSION \
+    ..
 fi
 
-if [[ $PYTHON_VERSION == 34 ]] && [[ $ENABLE_CONTRIB == 1 ]]; then
-  echo 'Config for Py34'
+if [[ $PYTHON_VERSION == 3* ]] && [[ $ENABLE_CONTRIB == 1 ]]; then
+  echo 'Config for Py3'
   cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D CMAKE_TOOLCHAIN_FILE=../../travis/toolchain_macos.cmake -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
     -D BUILD_opencv_python2=OFF -D BUILD_opencv_java=OFF -D BUILD_SHARED_LIBS=OFF -D WITH_LAPACK=OFF \
-    -D PYTHON3_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-    -D PYTHON3_LIBRARY=/usr/local/Cellar/python3/3.4.2_1/Frameworks/Python.framework/Versions/3.4/bin \
-    -D PYTHON3_INCLUDE_DIR=/Library/Frameworks/Python.framework/Versions/3.4/include/python3.4m \
     -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
-    -D BUILD_EXAMPLES=OFF ..
+    -D BUILD_EXAMPLES=OFF \
+    -D PYTHON3INTERP_FOUND=ON -DPYTHON3LIBS_FOUND=ON \
+    -D PYTHON3_EXECUTABLE=python \
+    -D PYTHON3_VERSION_STRING=$PYTHON_VERSION_STRING \
+    -D PYTHON3_INCLUDE_PATH=$PYTHON_INCLUDE_PATH \
+    -D PYTHON3_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
+    -D PYTHON3_NUMPY_INCLUDE_DIRS=$PYTHON_NUMPY_INCLUDE_DIRS \
+    -D PYTHON3_NUMPY_VERSION=$PYTHON_NUMPY_VERSION \
+    ..
 fi
-
-if [[ $PYTHON_VERSION == 35 ]] && [[ $ENABLE_CONTRIB == 1 ]]; then
-  echo 'Config for Py35'
-  cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D CMAKE_TOOLCHAIN_FILE=../../travis/toolchain_macos.cmake -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
-    -D BUILD_opencv_python2=OFF -D BUILD_opencv_java=OFF -D BUILD_SHARED_LIBS=OFF -D WITH_LAPACK=OFF \
-    -D PYTHON3_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-    -D PYTHON3_LIBRARY=/usr/local/Cellar/python3/3.5.1/Frameworks/Python.framework/Versions/3.5/bin \
-    -D PYTHON3_INCLUDE_DIR=/Library/Frameworks/Python.framework/Versions/3.5/include/python3.5m \
-    -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
-    -D BUILD_EXAMPLES=OFF ..
-fi
-
-if [[ $PYTHON_VERSION == 36 ]] && [[ $ENABLE_CONTRIB == 1 ]]; then
-  echo 'Config for Py36'
-  cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D CMAKE_TOOLCHAIN_FILE=../../travis/toolchain_macos.cmake -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
-    -D BUILD_opencv_python2=OFF -D BUILD_opencv_java=OFF -D BUILD_SHARED_LIBS=OFF -D WITH_LAPACK=OFF \
-    -D PYTHON3_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-    -D PYTHON3_LIBRARY=/usr/local/Cellar/python3/3.6.0/Frameworks/Python.framework/Versions/3.6/bin \
-    -D PYTHON3_INCLUDE_DIR=/Library/Frameworks/Python.framework/Versions/3.6/include/python3.6m \
-    -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
-    -D BUILD_EXAMPLES=OFF ..
-fi
-
 
 echo 'Begin build'
-make -j8
+
+if [[ $PYTHON_VERSION == 2* ]]; then
+  echo 'Build for Py2'
+  make -j8 opencv_python2
+fi
+
+if [[ $PYTHON_VERSION == 3* ]]; then
+  echo 'Build for Py3'
+  make -j8 opencv_python3
+fi
 
 # Moving back to opencv-python
 cd ../..
--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,7 +3,7 @@ set +e
 echo 'Begin build-wheel...'
 
 export PYTHON_VERSION=${PYTHON_VERSION/./}
-echo 'PYTHON_VERSION: '$PYTHON_VERSION
+echo 'PYTHON_VERSION: ' $PYTHON_VERSION
 
 ENABLE_CONTRIB=$(<contrib.enabled)
 
@@ -19,6 +19,20 @@ yum -y install curl-devel zlib-devel # need to build against system curl to get 
 make && make install
 cd ..
 
+echo "Detect Python paths for OpenCV"
+
+PYTHON_VERSION_STRING = $(python -c "from platform import python_version; print(python_version())")
+PYTHON_INCLUDE_PATH = $(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
+PYTHON_PACKAGES_PATH = $(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
+PYTHON_NUMPY_INCLUDE_DIRS = $(python -c "import os; os.environ['DISTUTILS_USE_SDK']='1'; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))")
+PYTHON_NUMPY_VERSION = $(python -c "import numpy; print(numpy.version.version)")
+
+echo "PYthon version string: $PYTHON_VERSION_STRING"
+echo "Python include path: $PYTHON_INCLUDE_PATH"
+echo "Python packages path: $PYTHON_PACKAGES_PATH"
+echo "Python numpy incude dirs: $PYTHON_NUMPY_INCLUDE_DIRS"
+echo "Python numpy version: $PYTHON_NUMPY_VERSION"
+
 # Begin build
 echo 'Begin build'
 cd opencv
@@ -27,57 +41,61 @@ mkdir build
 if [[ $PYTHON_VERSION == 2* ]] && [[ $ENABLE_CONTRIB == 0 ]]; then
   echo 'Config for Py2'
   cmake -H"." -B"build" -DCMAKE_BUILD_TYPE=Release -DBUILD_opencv_python3=OFF -DBUILD_opencv_java=OFF -DBUILD_SHARED_LIBS=OFF \
-    -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DWITH_IPP=OFF -DBUILD_DOCS=OFF \
-    -DINSTALL_C_EXAMPLES=OFF -DINSTALL_PYTHON_EXAMPLES=OFF \
-    -DPYTHON2INTERP_FOUND=ON -DPYTHON2LIBS_FOUND=ON \
-    -DPYTHON2_EXECUTABLE=python \
-    -DPYTHON2_VERSION_STRING=$(python -c "from platform import python_version; print(python_version())") \
-    -DPYTHON2_INCLUDE_PATH=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
-    -DPYTHON2_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-    -DPYTHON2_NUMPY_INCLUDE_DIRS=$(python -c "import os; os.environ['DISTUTILS_USE_SDK']='1'; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))") \
-    -DPYTHON2_NUMPY_VERSION=$(python -c "import numpy; print(numpy.version.version)")
+    -D WITH_IPP=OFF -D BUILD_DOCS=OFF \
+    -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
+    -D BUILD_EXAMPLES=OFF \
+    -D PYTHON2INTERP_FOUND=ON -DPYTHON2LIBS_FOUND=ON \
+    -D PYTHON2_EXECUTABLE=python \
+    -D PYTHON2_VERSION_STRING=$PYTHON_VERSION_STRING \
+    -D PYTHON2_INCLUDE_PATH=$PYTHON_INCLUDE_PATH \
+    -D PYTHON2_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
+    -D PYTHON2_NUMPY_INCLUDE_DIRS=$PYTHON_NUMPY_INCLUDE_DIRS \
+    -D PYTHON2_NUMPY_VERSION=$PYTHON_NUMPY_VERSION \
 fi
 
 if [[ $PYTHON_VERSION == 3* ]] && [[ $ENABLE_CONTRIB == 0 ]]; then
   echo 'Config for Py3'
   cmake -H"." -B"build" -DCMAKE_BUILD_TYPE=Release -DBUILD_opencv_python2=OFF -DBUILD_opencv_java=OFF -DBUILD_SHARED_LIBS=OFF \
-    -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DWITH_IPP=OFF -DBUILD_DOCS=OFF \
-    -DINSTALL_C_EXAMPLES=OFF -DINSTALL_PYTHON_EXAMPLES=OFF \
-    -DPYTHON3INTERP_FOUND=ON -DPYTHON3LIBS_FOUND=ON \
-    -DPYTHON3_EXECUTABLE=python \
-    -DPYTHON3_VERSION_STRING=$(python -c "from platform import python_version; print(python_version())") \
-    -DPYTHON3_INCLUDE_PATH=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
-    -DPYTHON3_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-    -DPYTHON3_NUMPY_INCLUDE_DIRS=$(python -c "import os; os.environ['DISTUTILS_USE_SDK']='1'; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))") \
-    -DPYTHON3_NUMPY_VERSION=$(python -c "import numpy; print(numpy.version.version)")
+    -D WITH_IPP=OFF -D BUILD_DOCS=OFF \
+    -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
+    -D BUILD_EXAMPLES=OFF \
+    -D PYTHON3INTERP_FOUND=ON -DPYTHON3LIBS_FOUND=ON \
+    -D PYTHON3_EXECUTABLE=python \
+    -D PYTHON3_VERSION_STRING=$PYTHON_VERSION_STRING \
+    -D PYTHON3_INCLUDE_PATH=$PYTHON_INCLUDE_PATH \
+    -D PYTHON3_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
+    -D PYTHON3_NUMPY_INCLUDE_DIRS=$PYTHON_NUMPY_INCLUDE_DIRS \
+    -D PYTHON3_NUMPY_VERSION=$PYTHON_NUMPY_VERSION \
 fi
 
 if [[ $PYTHON_VERSION == 2* ]] && [[ $ENABLE_CONTRIB == 1 ]]; then
   echo 'Config for Py2'
   cmake -H"." -B"build" -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules -DCMAKE_BUILD_TYPE=Release -DBUILD_opencv_python3=OFF -DBUILD_opencv_java=OFF -DBUILD_SHARED_LIBS=OFF \
-    -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DWITH_IPP=OFF -DBUILD_DOCS=OFF \
-    -DINSTALL_C_EXAMPLES=OFF -DINSTALL_PYTHON_EXAMPLES=OFF \
-    -DPYTHON2INTERP_FOUND=ON -DPYTHON2LIBS_FOUND=ON \
-    -DPYTHON2_EXECUTABLE=python \
-    -DPYTHON2_VERSION_STRING=$(python -c "from platform import python_version; print(python_version())") \
-    -DPYTHON2_INCLUDE_PATH=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
-    -DPYTHON2_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-    -DPYTHON2_NUMPY_INCLUDE_DIRS=$(python -c "import os; os.environ['DISTUTILS_USE_SDK']='1'; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))") \
-    -DPYTHON2_NUMPY_VERSION=$(python -c "import numpy; print(numpy.version.version)")
+    -D WITH_IPP=OFF -D BUILD_DOCS=OFF \
+    -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
+    -D BUILD_EXAMPLES=OFF \
+    -D PYTHON2INTERP_FOUND=ON -DPYTHON2LIBS_FOUND=ON \
+    -D PYTHON2_EXECUTABLE=python \
+    -D PYTHON2_VERSION_STRING=$PYTHON_VERSION_STRING \
+    -D PYTHON2_INCLUDE_PATH=$PYTHON_INCLUDE_PATH \
+    -D PYTHON2_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
+    -D PYTHON2_NUMPY_INCLUDE_DIRS=$PYTHON_NUMPY_INCLUDE_DIRS \
+    -D PYTHON2_NUMPY_VERSION=$PYTHON_NUMPY_VERSION \
 fi
 
 if [[ $PYTHON_VERSION == 3* ]] && [[ $ENABLE_CONTRIB == 1 ]]; then
   echo 'Config for Py3'
   cmake -H"." -B"build" -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules -DCMAKE_BUILD_TYPE=Release -DBUILD_opencv_python2=OFF -DBUILD_opencv_java=OFF -DBUILD_SHARED_LIBS=OFF \
-    -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DWITH_IPP=OFF -DBUILD_DOCS=OFF \
-    -DINSTALL_C_EXAMPLES=OFF -DINSTALL_PYTHON_EXAMPLES=OFF \
-    -DPYTHON3INTERP_FOUND=ON -DPYTHON3LIBS_FOUND=ON \
-    -DPYTHON3_EXECUTABLE=python \
-    -DPYTHON3_VERSION_STRING=$(python -c "from platform import python_version; print(python_version())") \
-    -DPYTHON3_INCLUDE_PATH=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
-    -DPYTHON3_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
-    -DPYTHON3_NUMPY_INCLUDE_DIRS=$(python -c "import os; os.environ['DISTUTILS_USE_SDK']='1'; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))") \
-    -DPYTHON3_NUMPY_VERSION=$(python -c "import numpy; print(numpy.version.version)")
+    -D WITH_IPP=OFF -D BUILD_DOCS=OFF \
+    -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF \
+    -D BUILD_EXAMPLES=OFF \
+    -D PYTHON3INTERP_FOUND=ON -DPYTHON3LIBS_FOUND=ON \
+    -D PYTHON3_EXECUTABLE=python \
+    -D PYTHON3_VERSION_STRING=$PYTHON_VERSION_STRING \
+    -D PYTHON3_INCLUDE_PATH=$PYTHON_INCLUDE_PATH \
+    -D PYTHON3_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
+    -D PYTHON3_NUMPY_INCLUDE_DIRS=$PYTHON_NUMPY_INCLUDE_DIRS \
+    -D PYTHON3_NUMPY_VERSION=$PYTHON_NUMPY_VERSION \
 fi
 
 if [[ $PYTHON_VERSION == 2* ]]; then
