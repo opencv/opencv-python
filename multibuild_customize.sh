2ded5f0e56714db3a947ade056321d54410c3a5b
Ivan Pozdeev <vano@mail.mipt.ru>
2017-12-20 07:38:03 +0300

    use custom multibuild

--- multibuild_customize.sh
+++ multibuild_customize.sh
@@ -4,34 +4,5 @@ export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 set -x
 REPO_DIR=$(dirname "${BASH_SOURCE[0]}")
 BUILD_COMMIT=$(git rev-parse HEAD)
-DOCKER_IMAGE=quay.io/skvark/manylinux_$plat
+DOCKER_IMAGE='quay.io/skvark/manylinux_$plat'
 
-function build_multilinux {
-    # Runs passed build commands in manylinux container
-    #
-    # Depends on
-    #     MB_PYTHON_VERSION
-    #     UNICODE_WIDTH (optional)
-    #     BUILD_DEPENDS (optional)
-    #     MANYLINUX_URL (optional)
-    #     WHEEL_SDIR (optional)
-    local plat=$1
-    [ -z "$plat" ] && echo "plat not defined" && exit 1
-    local build_cmds="$2"
-    local docker_image=${DOCKER_IMAGE:-quay.io/pypa/manylinux1_$plat}
-    retry docker pull $docker_image
-    docker run --rm \
-        -e BUILD_COMMANDS="$build_cmds" \
-        -e PYTHON_VERSION="$MB_PYTHON_VERSION" \
-        -e UNICODE_WIDTH="$UNICODE_WIDTH" \
-        -e BUILD_COMMIT="$BUILD_COMMIT" \
-        -e WHEEL_SDIR="$WHEEL_SDIR" \
-        -e MANYLINUX_URL="$MANYLINUX_URL" \
-        -e BUILD_DEPENDS="$BUILD_DEPENDS" \
-        -e USE_CCACHE="$USE_CCACHE" \
-        -e REPO_DIR="$repo_dir" \
-        -e PLAT="$PLAT" \
-        -v $PWD:/io \
-        -v $HOME:/parent-home \
-        $docker_image /io/$MULTIBUILD_DIR/docker_build_wrap.sh
-}
