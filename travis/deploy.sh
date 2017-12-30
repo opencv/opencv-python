58d6ee4fb11b4e1a8d1af4705aa054488c07b565
Ivan Pozdeev <vano@mail.mipt.ru>
2017-12-30 22:54:57 +0300

    don't do anything if upload not needed

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,12 +1,12 @@
-pip install twine
-
-if [[ $ENABLE_CONTRIB == 0 ]]; then
-	echo "This is default build. Deployment will be done to to PyPI entry opencv-python."
-else
-	echo "This is contrib build. Deployment will be done to to PyPI entry opencv-contrib-python."
-fi
-
 if [ -n "$TRAVIS_TAG" ]; then
+    pip install twine
+
+    if [[ $ENABLE_CONTRIB == 0 ]]; then
+        echo "This is default build. Deployment will be done to to PyPI entry opencv-python."
+    else
+        echo "This is contrib build. Deployment will be done to to PyPI entry opencv-contrib-python."
+    fi
+
 	twine upload -u ${USER} -p ${PASS} --skip-existing ${TRAVIS_BUILD_DIR}/wheelhouse/opencv*;
 else
 	echo "Tag not set, deployment skipped.";
