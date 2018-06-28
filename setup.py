f6e2eea80a312e0909577f9ce05e0e67ada62cc0
Olli-Pekka Heinisuo <op.heinisuo@gmail.com>
2018-06-28 14:29:02 +0300

    test OpenCV GUI patches

--- setup.py
+++ setup.py
@@ -109,6 +109,9 @@ def main():
     # OS-specific components
     if (sys.platform == 'darwin' or sys.platform.startswith('linux')) and not build_headless:
         cmake_args.append("-DWITH_QT=4")
+        # Apply Qt namespace patches to OpenCV GUI code
+        subprocess.check_call(["patch", "-p0", "<", "patches/patch1.patch"])
+        subprocess.check_call(["patch", "-p0", "<", "patches/patch2.patch"])
 
     if build_headless:
         # it seems that cocoa cannot be disabled so on macOS the package is not truly headless
