a62380b389af5bd6096c5a11dc633aedac730934
Olli-Pekka Heinisuo <op.heinisuo@gmail.com>
2018-07-15 19:35:26 +0300

    xcode9.3

--- config.sh
+++ config.sh
@@ -25,6 +25,8 @@ function pre_build {
 
     brew update
 
+    export CXXFLAGS=-mmacosx-version-min=10.10
+
     echo 'Installing QT4'
     brew tap | grep -qxF cartr/qt4 || brew tap -v cartr/qt4
     brew tap --list-pinned | grep -qxF cartr/qt4 || brew tap-pin -v cartr/qt4
