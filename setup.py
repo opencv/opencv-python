4b156057ec7240bde30882990fd5183bc534e430
Olli-Pekka Heinisuo <op.heinisuo@gmail.com>
2018-07-20 13:01:08 +0300

    add missing identifier for 3.7

--- setup.py
+++ setup.py
@@ -188,6 +188,7 @@ def main():
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Programming Language :: C++',
           'Programming Language :: Python :: Implementation :: CPython',
           'Topic :: Scientific/Engineering',
