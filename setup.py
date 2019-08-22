2700ec938f998210add5eb65f9387a332db29cc9
Olli-Pekka Heinisuo <op.heinisuo@gmail.com>
2019-08-22 21:49:00 +0300

    opencv 4.1.1 does not seem to support macOS 10.7 anymore, bump to 10.8

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ def main():
         cmake_args.append("-DWITH_LAPACK=OFF")  # Some OSX LAPACK fns are incompatible, see
                                                 # https://github.com/skvark/opencv-python/issues/21
         cmake_args.append("-DCMAKE_CXX_FLAGS=-stdlib=libc++")
-        cmake_args.append("-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.7")
+        cmake_args.append("-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.8")
 
     if sys.platform.startswith('linux'):
         cmake_args.append("-DWITH_IPP=OFF")   # tests fail with IPP compiled with
