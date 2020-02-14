d1a71597a42a635a218292b94c0e826a2224c4ae
David Lechner <david@lechnology.com>
2020-02-14 14:40:22 -0600

    fix missing Qt plugin on macOS

--- setup.py
+++ setup.py
@@ -128,6 +128,7 @@ def main():
 
     if sys.platform == 'darwin' and not build_headless:
         cmake_args.append("-DWITH_QT=5")
+        rearrange_cmake_output_data['cv2.qt.plugins.platforms'] = [(r'lib/qt/plugins/platforms/libqcocoa\.dylib')]
 
     if build_headless:
         # it seems that cocoa cannot be disabled so on macOS the package is not truly headless
@@ -144,6 +145,7 @@ def main():
                                                 # https://github.com/skvark/opencv-python/issues/21
         cmake_args.append("-DCMAKE_CXX_FLAGS=-stdlib=libc++")
         cmake_args.append("-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9")
+        subprocess.check_call("patch -p1 < patches/patchQtPlugins", shell=True)
 
     if sys.platform.startswith('linux'):
         cmake_args.append("-DWITH_IPP=OFF")   # tests fail with IPP compiled with
