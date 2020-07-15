d434c0113e112f7cc0e1d1e4beb1637ffa29253f
Olli-Pekka Heinisuo <op.heinisuo@gmail.com>
2020-07-15 23:48:44 +0300

    set travis user manually

--- travis_config.sh
+++ travis_config.sh
@@ -88,7 +88,7 @@ function pre_build {
     echo "Running for OSX"
 
     local CACHE_STAGE; (echo "$TRAVIS_BUILD_STAGE_NAME" | grep -qiF "final") || CACHE_STAGE=1
-
+    export USER=travis
     #after the cache stage, all bottles and Homebrew metadata should be already cached locally
     if [ -n "$CACHE_STAGE" ]; then
         brew update
