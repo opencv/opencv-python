40e0e3b8c235386c003f8ea5d0a9d9ea968a06cd
Andrey Senyaev <andrey.senyaev@xperience.ai>
2021-10-01 13:51:43 +0300

    Fixed aarch64 openblas installation

--- travis_config.sh
+++ travis_config.sh
@@ -95,7 +95,8 @@ function pre_build {
   if [ -n "$IS_OSX" ]; then
     brew install lapack
   else
-    yum install -y lapack-devel openblas-devel
+    # epel-release need for aarch64 to get openblas packages
+    yum install -y lapack-devel epel-release && yum install -y openblas-devel
     cp /usr/include/lapacke/lapacke*.h /usr/include/
     curl https://raw.githubusercontent.com/xianyi/OpenBLAS/v0.3.3/cblas.h -o /usr/include/cblas.h
   fi
