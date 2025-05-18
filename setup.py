8e63c5b47cda57f7dfa074e61d61199e50898ee4
Ivan Avdeev <avdeeff@internet.ru>
2025-05-18 15:45:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -193,7 +193,6 @@ def main():
             "-DBUILD_DOCS=OFF",
             "-DPYTHON3_LIMITED_API=ON",
             "-DBUILD_OPENEXR=ON",
-            "-DWITH_MSMF=OFF",
         ]
         + (
             # CMake flags for windows/arm64 build
