52b491c3c23669c9f88339ca5ab1f41f212f8f53
Ivan Avdeev <avdeeff@internet.ru>
2025-06-03 18:45:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -115,6 +115,12 @@ def main():
             else []
         )
         +
+        "openblas": (
+        [r"openblas/bin/libopenblas\d*\.dll"]
+        if os.name == "nt"
+        else []
+        )
+        +
         # In Windows, in python/X.Y/<arch>/; in Linux, in just python/X.Y/.
         # Naming conventions vary so widely between versions and OSes
         # had to give up on checking them.
