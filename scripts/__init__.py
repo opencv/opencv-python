4e4505a2774a50ce1ad5a46b7673f71257d77265
Mulham Fetna <molham0994384445@gmail.com>
2026-05-09 21:02:40 +0300

    docs: add multiprocessing fork safety warning
    Add documentation about OpenCV not being fork-safe and the
    'corrupted double-linked list' issue that can occur when using
    cv2 functions after fork().
    
    Fixes: #1166

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,3 +1,22 @@
+"""
+OpenCV Python bindings
+
+This module provides Python bindings for OpenCV.
+
+Important notes for multiprocessing:
+- OpenCV is not fork-safe. Using cv2 functions after fork() can cause
+  "corrupted double-linked list" errors and memory corruption.
+- If you use multiprocessing with fork (the default on Linux/macOS),
+  either:
+  1. Use spawn mode: multiprocessing.set_start_method('spawn')
+  2. Call cv2.setNumThreads(0) in each worker before using cv2
+- See: https://github.com/opencv/opencv-python/issues/1166
+"""
+
+import os
+import sys
+import warnings
+
 PYTHON_EXTENSIONS_PATHS = [
     LOADER_DIR
 ] + PYTHON_EXTENSIONS_PATHS
